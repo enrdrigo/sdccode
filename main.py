2818e1365af67b3b1da42a46d4e52891324d4ba7
enrdrigo <72918800+enrdrigo@users.noreply.github.com>
2021-05-04 23:35:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ from modules.computestatdc import computestatdc
 #INITIALIZATION
 
 start=time.time()
-#I MUST INDICATE WHERE ARE THE DATA
+#MUST INDICATE WHERE ARE THE DATA
 root=''
 filename='dump1.1fs.lammpstrj'
-#GET THE NUMBER OF PARTICLES IN THE SIMULATION
+#GETS THE NUMBER OF PARTICLES IN THE SIMULATION
 Npart=initialize.getNpart(filename, root)
-#IF NOT ALREADY DONE SAVE THE DATA IN A BINARY FORMAT SO THAT IN THE FUTURE I READ JUST THE BINARY
+#IF NOT ALREADY DONE SAVE THE DATA IN A BINARY FORMAT SO THAT IN THE FUTURE READS JUST THE BINARY
 if os.path.exists(root+'data'+'{}'.format(Npart)+'.npy'):
     filebin='data'+'{}'.format(Npart)+'.npy'
 else:
     filebin=initialize.saveonbin(filename, root, Npart)#JUST DO ONCE!!!! READ DATA FROM THE BINARY INSTEAD
-#GET THE DIMENSIONS OF THE SIMULATION BOX
+#GETS THE DIMENSIONS OF THE SIMULATION BOX
 Lato, Lmin=initialize.getBoxboundary(filename, root)
-#GET THE NUMBER OF SNAPSHOT OF THE SIMULATION. RESHAPE THE ARRAY OF THE DATA SO THAT WE HAVE A MATRIX WITH THE
+#GETS THE NUMBER OF SNAPSHOT OF THE SIMULATION. RESHAPE THE ARRAY OF THE DATA SO THAT WE HAVE A MATRIX WITH THE
 # COORDINATES AND THE CHARGES FOR EACH SNAPSHOT
 data_arrayy, nsnapshot=initialize.getDatainshape_andNsnap(filebin, root, Npart)
 
@@ -37,7 +37,7 @@ print('Initialization done in {:10.5f}'. format(time.time()-start)+'s')
 #CALCULATION OF THE DIPOLES
 
 start1=time.time()
-#COMPUTE THE MATRIX OF THE MOLECULAR DIPOLES, THE CENTER OF MASS OF THE MOLECULE, THE ATOMIC CHARGES AND
+#COMPUTES THE MATRIX OF THE MOLECULAR DIPOLES, THE CENTER OF MASS OF THE MOLECULE, THE ATOMIC CHARGES AND
 #THE POSITION OF THE CHARGES (IN TIP4P/2005 THE OXY CHARGE IS IN A DIFFERENT POSITION THAN THE OXY ITSELF)
 dipmol,cdmol, chat, pos=dipole.staticdc(Npart, Lato,Lmin, nsnapshot, data_arrayy)
 print("Molecular dipoles, molecular positions, charges and charge positions for the trajectory computed in {:10.5f}".format(time.time()-start1)+'s')
@@ -48,7 +48,7 @@ print("Molecular dipoles, molecular positions, charges and charge positions for 
 
 start2=time.time()
 nk=20
-#COMPUTE THE STATIC DIELECTRIC CONSTANT FOR NK VALUES OF THE G VECTOR IN THE (1,0,0) DIRECTION: 2\PI/LATO*(J,0,0), J=1,..NK
+#COMPUTES THE STATIC DIELECTRIC CONSTANT FOR NK VALUES OF THE G VECTOR IN THE (1,0,0) DIRECTION: 2\PI/LATO*(J,0,0), J=1,..NK
 e0pol, e0ch=computestatdc(nk,dipmol, cdmol,chat,pos, Lato, nsnapshot)
 #PRINT THE DIELECTRIC CONSTANT COMPUTED VIA THE POLARIZATION AND VIA THE CHARGES AS A FUNCTION OF G AND SAVE IN A FILE
 file='{}'.format(Npart)+'{}'.format(nk)+'dielconst.dat'
