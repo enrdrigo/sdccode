fdf4723a79287c7e66330b347d2769282750e25e
enrdrigo <enr.drigo@gmail.com>
2021-05-08 16:59:41 +0200

    numba and dpcf

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import numpy as np
 import os
-# from numba import njit, prange, objmode
+from numba import njit, prange, objmode
+import matplotlib.pyplot as plt
 import time
 from modules import initialize
 from modules import dipole
-from modules.computestatdc import computestatdc
+from modules import computestatdc
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -12,7 +13,7 @@ from modules.computestatdc import computestatdc
 
 start = time.time()
 # INDICATES WHERE ARE THE DATA
-root = ''
+root = '.'
 filename = 'dump1.1fs.lammpstrj'
 # GETS THE NUMBER OF PARTICLES IN THE SIMULATION
 Npart = initialize.getNpart(filename, root)
@@ -49,12 +50,13 @@ start2 = time.time()
 nk = 20
 # COMPUTES THE STATIC DIELECTRIC CONSTANT FOR NK VALUES OF THE G VECTOR IN THE (1,0,0) DIRECTION:
 # 2\PI/LATO*(J,0,0), J=1,..NK
-e0pol, e0ch = computestatdc(nk, dipmol, cdmol, chat, pos, Lato, nsnapshot)
+e0pol, e0ch = computestatdc.computestatdc(nk, dipmol, cdmol, chat, pos, Lato, nsnapshot)
 print('Static dielectric constant for {}'.format(nk)+' values of k computed in {:10.5f}'.format(time.time()-start2)+'s')
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 # PRINT THE DIELECTRIC CONSTANT COMPUTED VIA THE POLARIZATION AND VIA THE CHARGES AS A FUNCTION OF G AND SAVE IN A FILE
+
 file = '{}'.format(Npart)+'{}'.format(nk)+'dielconst.dat'
 f = open(file, 'w+')
 print("k\t"+'e0pol_xx\t'+'e0pol_yy\t'+'e0pol_zz\t'+'e0ch')
@@ -64,4 +66,21 @@ for j in range(nk):
     print('{}\t'.format(j)+'{:10.3f}\t'.format(e0pol[j][0])+'{:10.3f}\t'.format(e0pol[j][1])+'{:10.3f}\t'.format(e0pol[j][2])+'{:10.3f}'.format(e0ch[j]))
     f.write('{}\t'.format(j)+'{:10.3f}\t'.format(e0pol[j][0])+'{:10.3f}\t'.format(e0pol[j][1])+'{:10.3f}\t'.format(e0pol[j][2])+'{:10.3f}\n'.format(e0ch[j]))
 print('The static dielectric constants are saved in '+root+file)
+print('The static dielectric constant for {}'.format(nk)+' values of k computed in : {:10.5f}'.format(time.time()-start2)+'s')
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# COMPUTES THE DIPOLES PAIR CORRELATION FUNCTION AT GMIN IN THE (G,0,0) DIRECTION AND SAVE IT IN A FILE
+
+nk = 10
+G = 0
+start2 = time.time()
+file = '{}'.format(Npart)+'{}'.format(G)+'dippcf.dat'
+print('The dipole pair correlation function for G=({}, 0, 0)'.format(G)+' is saved in '+root+file)
+print('r\t'+'c_m_x\t'+'c_m_y\t'+'c_m_z\n')
+gk = computestatdc.dip_paircf(G, nk, dipmol, cdmol, Lato, nsnapshot)
+f = open(file, 'w+')
+for i in range(nk):
+    f.write('{:10.5f}\t'.format((i*(Lato - 2)/2/nk +2)/0.529) + '{:10.5f}\t'.format(gk[i][0]) + '{:10.5f}\t'.format(gk[i][1]) + '{:10.5f}\n'.format(gk[i][2]))
+print('The dipole pair correlation function for G=({}, 0, 0)'.format(G)+'  computed in : {:10.5f}'.format(time.time()-start2)+'s')
 print('Total elapsed time: {:10.5f}'.format(time.time()-start)+'s')
--- modules/computestatdc.py
+++ modules/computestatdc.py
@@ -1,5 +1,6 @@
 import numpy as np
-#from numba import njit, prange, objmode
+import time
+from numba import njit# prange, objmode
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -7,22 +8,92 @@ import numpy as np
 # DIRECTION (1,0,0). THE SDC IS CALCULATED VIA THE AVERAGE VALUE OF THE MODULUS SQUARED OF THE FOURIER TRANFORM OF THE
 # POLARIZATION IN G OR VIA THE MODULUS SQUARED OF THE FOURIER TRANSORM OF THE CHARGE IN G DIVIDED BY THE MODULUS OF G.
 
-# @njit(fastmath=True, parallel=True)
+@njit(fastmath=True, parallel=True)
 def computestatdc(nk, dipmol, cdmol,chat,pos, L, nsnap):
     e0pol=np.zeros((nk,3))
     e0ch=np.zeros(nk)
     for j in range(nk):
         e0pol[j][0]=1+(16.022**2)*np.sum(np.abs(np.sum(np.transpose(dipmol)[0] *\
-                    np.exp(1j*np.transpose(cdmol)[0]*(j*2*np.pi/L)), axis=0))**2)/nsnap\
+                    np.exp(1j*(np.transpose(cdmol)[0]*(j*2*np.pi/L))), axis=0))**2)/nsnap\
                     *1.0e5/(L**3*1.38*300*8.854)
         e0pol[j][1]=1+(16.022**2)*np.sum(np.abs(np.sum(np.transpose(dipmol)[1] *\
-                    np.exp(1j*np.transpose(cdmol)[0]*(j*2*np.pi/L)), axis=0))**2)/nsnap\
+                    np.exp(1j*(np.transpose(cdmol)[0]*(j*2*np.pi/L))), axis=0))**2)/nsnap\
                     *1.0e5/(L**3*1.38*300*8.854)
         e0pol[j][2]=1+(16.022**2)*np.sum(np.abs(np.sum(np.transpose(dipmol)[2] *\
-                    np.exp(1j*np.transpose(cdmol)[0]*(j*2*np.pi/L)), axis=0))**2)/nsnap\
+                    np.exp(1j*(np.transpose(cdmol)[0]*(j*2*np.pi/L))), axis=0))**2)/nsnap\
                     *1.0e5/(L**3*1.38*300*8.854)
         e0ch[j]=1+(16.022**2)*np.mean(np.abs(np.sum(np.transpose(chat) *\
-                np.exp(1j*np.transpose(pos)[0]*(j*2*np.pi/L)), axis=0))**2)\
+                np.exp(1j*(np.transpose(pos)[0]*(j*2*np.pi/L))), axis=0))**2)\
                 /((j*2*np.pi/L+ np.sqrt(3)*2*np.pi/L*1e-8)**2)*1.0e5/(L**3*1.38*300*8.854)
     return e0pol, e0ch
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+def dip_paircf(G, nk, dipmol, cdmol, L, nsnap):
+    nmol = np.shape(dipmol)[1]
+    distcdm = np.zeros((nmol, nmol, nsnap))
+    diffcdm = np.zeros((nmol, 3, nmol, nsnap))
+    dipsq=np.zeros((nmol, 3, nmol, nsnap), dtype=np.complex_)
+    gk = np.zeros((nk, 3))
+    sigma = 0.04
+    cm = np.zeros((nmol, 3, nmol, nsnap), dtype=np.complex_)
+    # DIPMOL, CDMOL HAVE DIMENTION (NSNAP, NMOL, 3)
+    cdmol = np.transpose(cdmol, (1, 2, 0))
+    dipmol = np.transpose(dipmol, (1, 2, 0))
+    tcdmol = np.zeros((3,nmol, nsnap))
+    tcdmol = np.transpose(cdmol, (1, 0, 2))
+    tdipmol = np.zeros((3, nmol, nsnap))
+    tdipmol = np.transpose(dipmol, (1, 0, 2))
+    for i in range(nk):
+
+        r = i * (L - 2) / 2 / nk + 2
+        for s in range(nmol):
+
+            diffcdm[s, :, :, :] = cdmol[s, :, None, :]-tcdmol[:, :, :]
+
+            distcdm[s, :, :] = np.sqrt(np.sum(diffcdm[s, :, :, :]**2, axis=0))
+
+            dipsq[s, :, :, :] = dipmol[s, :, None, :]*tdipmol[:, :, :] * np.exp(1j * diffcdm[s, 0, None, :, :] * (G * 2 * np.pi / L))
+
+            dipsq[s, :, s, :] = 0
+
+            cm[s, :, :, :] = dipsq[s, :, :, :]*np.exp(-(r-distcdm[s, None, :, :])**2/sigma**2)
+
+        gk[i] = np.real(np.sum(np.sum(np.sum(cm, axis=0), axis=1)/nmol, axis=1)/nsnap/r**2/2*np.pi/sigma**2)
+        print('{:10.5f}\t'.format(r/0.529)+'{:10.5f}\t'.format(gk[i][0])+'{:10.5f}\t'.format(gk[i][1])\
+              +'{:10.5f}\n'.format(gk[i][2]))
+
+    return gk
+
+
+# cm = np.zeros((3, nmol, nsnap), dtype=np.complex_)
+# for s in range(nk):
+#     r = (s+1)*L/nk
+#     for i in range(nmol):
+#         diffcdm= np.transpose(cdmol, [1, 0, 2]) - np.transpose(cdmol, [1, 0, 2])[i]
+#         distcdm = np.sqrt(np.sum(diffcdm**2, axis=2))
+#         dipsq = np.transpose(np.transpose(np.transpose(dipmol, [1, 0, 2]) * np.transpose(dipmol, [1, 0, 2])[i]) \
+#                 * np.exp(1j * np.transpose(diffcdm, [2, 1, 0])[0] * (1 * 2 * np.pi / L)))
+#         # dipsq = np.transpose(dipmol, [1, 0, 2])*np.transpose(dipmol, [1, 0, 2])[i]\
+#         #         * np.exp(1j*np.transpose(diffcdm, [2, 0, 1])[0]*(1*2*np.pi/L))
+#         dipsq[i, :] = 0
+#         cm[:, i, :] = np.sum(np.transpose(dipsq, [2, 0, 1])*np.exp(-(r-distcdm)**2/sigma**2), axis=1)
+#     gk[s] = np.real(np.sum(np.sum(cm, axis=1)/nmol, axis=1)/nsnap/r**2/2*np.pi/sigma**2)
+#     print(r/0.529, gk[s])
+
+#    for i in range(nmol):
+#        # 1=NMOL, 0=NSNAP, 2=3
+#        diffcdm= np.transpose(cdmol, [1, 0, 2]) - np.transpose(cdmol, [1, 0, 2])[i]
+#        distcdm = np.sqrt(np.sum(diffcdm**2, axis=2))
+#        # 1=NMOL, 0=NSNAP, 2=3
+#        dipsq = np.transpose(np.transpose(np.transpose(dipmol, [1, 0, 2]) * np.transpose(dipmol, [1, 0, 2])[i]) \
+#                * np.exp(1j * np.transpose(diffcdm, [2, 1, 0])[0] * (0 * 2 * np.pi / L))) # 2=3, 1=NSNAP, 0=NMOL
+#        dipsq[i] = 0
+#        # 2=3, 1=NMOL, 1=NSNAP
+#        cm[:, :, i, :] = np.sum(np.transpose(dipsq, [2, 0, 1])*np.exp(-(r-distcdm)**2/sigma**2), axis=2)
+#    gk = np.real(np.sum(np.sum(cm, axis=2)/nmol, axis=2)/nsnap/rr**2/2*np.pi/sigma**2)
+
+
+
+
 
