3292b02ee2d4065e93133b0b00e84295bc5a3f01
enrdrigo <enr.drigo@gmail.com>
2021-07-12 16:46:53 +0200

    update 12/7 autocorr f

--- modules/computestatdc.py
+++ modules/computestatdc.py
@@ -27,6 +27,10 @@ def computestatdc(nk, dipmol, cdmol, chat, pos, L, nsnap):
                 / ((j * 2 * np.pi / L + np.sqrt(3) * 2 * np.pi / L * 1e-8)**2) * 1.0e5 / (L**3 * 1.38 * 300 * 8.854)
     return e0pol, e0ch
 
+
+# ----------------------------------------------------------------------------------------------------------------------
+# COMPUTES THE STATIC THERMOPOLARIZATION COEFFICIENT (STPC) AT A GIVEN PHYSICAL G VECTOR. HERE WE RESTRICT OURSELVES IN THE
+# DIRECTION (1,0,0). WE MUST DIVIDE BY THE STATIC RESPONSE (LONGITUDINAL).
 @njit(fastmath=True, parallel=True)
 def thermopolcoeff(nk, chat, enat, em, pos, posatomic, L, nsnap):
     tpc = np.zeros(nk, dtype=np.complex_)
@@ -34,7 +38,7 @@ def thermopolcoeff(nk, chat, enat, em, pos, posatomic, L, nsnap):
         tpc[j] = np.mean(np.sum(np.transpose(chat)*np.exp(1j * (np.transpose(pos)[0] * (j * 2 * np.pi / L))), axis=0) *\
                 (np.sum(np.transpose(enat)*np.exp(-1j * (np.transpose(posatomic)[0] * (j * 2 * np.pi / L))), axis=0)-em))\
                  / ((j * 2 * np.pi / L + np.sqrt(3) * 2 * np.pi / L * 1e-8)**2) * \
-                 16.022 * 1.0e-30 * 4184 / 6.02214e23 * 1.0e-10 / ((L) ** 3 * 1.0e-30 * 1.38e-23 * 300 * 300 * 62.5 * 8.854 * 1.0e-12)
+                 16.022 * 1.0e-30 * 4184 / 6.02214e23 * 1.0e-10 / ((L) ** 3 * 1.0e-30 * 1.38e-23 * 300 * 300 * 8.854 * 1.0e-12)
 
     return tpc
 
@@ -93,32 +97,6 @@ def dip_paircf(G, nk, rdipmol, rcdmol, tdipmol, tcdmol, L, nsnap):
 
     return gk, stdgk
 
-# cm = np.zeros((3, nmol, nsnap), dtype=np.complex_)
-# for s in range(nk):
-#     r = (s+1)*L/nk
-#     for i in range(nmol):
-#         diffcdm= np.transpose(cdmol, [1, 0, 2]) - np.transpose(cdmol, [1, 0, 2])[i]
-#         distcdm = np.sqrt(np.sum(diffcdm**2, axis=2))
-#         dipsq = np.transpose(np.transpose(np.transpose(dipmol, [1, 0, 2]) * np.transpose(dipmol, [1, 0, 2])[i]) \
-#                 * np.exp(1j * np.transpose(diffcdm, [2, 1, 0])[0] * (1 * 2 * np.pi / L)))
-#         # dipsq = np.transpose(dipmol, [1, 0, 2])*np.transpose(dipmol, [1, 0, 2])[i]\
-#         #         * np.exp(1j*np.transpose(diffcdm, [2, 0, 1])[0]*(1*2*np.pi/L))
-#         dipsq[i, :] = 0
-#         cm[:, i, :] = np.sum(np.transpose(dipsq, [2, 0, 1])*np.exp(-(r-distcdm)**2/sigma**2), axis=1)
-#     gk[s] = np.real(np.sum(np.sum(cm, axis=1)/nmol, axis=1)/nsnap/r**2/2*np.pi/sigma**2)
-#     print(r/0.529, gk[s])
-
-#    for i in range(nmol):
-#        # 1=NMOL, 0=NSNAP, 2=3
-#        diffcdm= np.transpose(cdmol, [1, 0, 2]) - np.transpose(cdmol, [1, 0, 2])[i]
-#        distcdm = np.sqrt(np.sum(diffcdm**2, axis=2))
-#        # 1=NMOL, 0=NSNAP, 2=3
-#        dipsq = np.transpose(np.transpose(np.transpose(dipmol, [1, 0, 2]) * np.transpose(dipmol, [1, 0, 2])[i]) \
-#                * np.exp(1j * np.transpose(diffcdm, [2, 1, 0])[0] * (0 * 2 * np.pi / L))) # 2=3, 1=NSNAP, 0=NMOL
-#        dipsq[i] = 0
-#        # 2=3, 1=NMOL, 1=NSNAP
-#        cm[:, :, i, :] = np.sum(np.transpose(dipsq, [2, 0, 1])*np.exp(-(r-distcdm)**2/sigma**2), axis=2)
-#    gk = np.real(np.sum(np.sum(cm, axis=2)/nmol, axis=2)/nsnap/rr**2/2*np.pi/sigma**2)
 
 
 
