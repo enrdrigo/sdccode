98c7791fe8ff7e3267b810d33216e2ba0450c4b8
enrdrigo <72918800+enrdrigo@users.noreply.github.com>
2021-08-22 16:13:47 +0200

    update 22/8

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ filename = 'dump1.1fs.lammpstrj'
 # GETS THE NUMBER OF PARTICLES IN THE SIMULATION
 Npart = initialize.getNpart(filename, root)
 # IF NOT ALREADY DONE SAVE THE DATA IN A BINARY FORMAT SO THAT IN THE FUTURE READS JUST THE BINARY
-if os.path.exists(root+filename+'{}'.format(Npart)+'.bin'):
-    filebin = filename +'{}'.format(Npart)+'.bin'
+if os.path.exists(root+filename+'{}'.format(Npart)+'.npz'):
+    filebin = filename +'{}'.format(Npart)+'.npz'
 else:
-    print('The file is not alreay saved in a .bin format, wait for the conversion.')
+    print('The file is not alreay saved in a .npz format, wait for the conversion.')
     filebin = initialize.saveonbin(filename, root, Npart)  # JUST DO ONCE!!!! READ DATA FROM THE BINARY INSTEAD
 # GETS THE DIMENSIONS OF THE SIMULATION BOX
 Lato, Lmin = initialize.getBoxboundary(filename, root)
--- testnltt.py
+++ testnltt.py
@@ -1,19 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 from modules import initialize
 from modules import dipole
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
-from scipy.fft import fft, ifft
+from scipy.fft import fft
 import os
-
-
-# In[2]:
 
 
 root='../'
@@ -24,26 +14,24 @@ print('lato cella', L)
 f=open('file.out', '+w')
 f.write('lato cella'+'{}\n'.format(L))
 f.close()
-# In[3]:
 
 
-if os.path.exists(root+filename+'{}'.format(Npart)+'.bin'):
-    filebin = filename+'{}'.format(Npart)+'.bin'
+if os.path.exists(root+filename+'{}'.format(Npart)+'.npz'):
+    filebin = filename+'{}'.format(Npart)+'.npz'
 else:
     filebin = initialize.saveonbin(filename, root, Npart)
 
 
-# In[4]:
+nsnap = initialize.getNsnap(filebin, root, Npart)
 
-
-nsnap=initialize.getNsnap(filebin, root, Npart)
 
 g = open('file.out', '+w')
 g.write('got nsnap\n')
 g.close()
 
-# In[5]:
+
 dip_mol, cdmol, ch_at, pos_ch, en_at, em, endip, pos_at=dipole.computedipole(Npart, L, Linf,nsnap, filebin, root, 0.125)
+
 
 nk=20
 nplot=4
@@ -55,6 +43,7 @@ for i in range(nk):
 qm=np.sum(q1, axis=1)/nsnap
 q=np.transpose(np.transpose(q1)-qm)
 
+
 corr=np.zeros((nk, nsnap), dtype=np.complex_)
 result=np.zeros(2*nsnap, dtype=np.complex_)
 for j in range(nk):
@@ -63,25 +52,25 @@ for j in range(nk):
     v = 0
     v = [result[i] /( len(q[j,:])-abs( i - (len(q[j,:])) +1 ) ) for i in range(len(result))]
     corr[j] = np.array(v[int(result.size/2):])
+
+
 xx=np.linspace(0,nsnap-1,nsnap)
 ft=np.zeros((nk, int(nsnap/4)), dtype=np.complex_)
 for i in range(nk):
     ft[i]=fft(np.real(corr[i][:int(nsnap/4)]))
-print(np.real(ft[0][:]).shape)
+
 
 chi=np.var(q1, axis=1)
 nltt=np.zeros(nk-1)
 for i in range(1,nk):
     nltt[i-1]=chi[i]/(ft[i, 0]*(2*(i)*np.pi/L)**2)*1.e-5
     print(i, nltt[i-1], chi[i], ft[i, 0])
+
+
 xk=np.linspace(1,nk, nk-1)
 f=open('nltt.dat', 'w+')
 for i in range(nk-1):
     f.write('{}\t'.format(xk[i]*2*np.pi/L*10)+'{}\n'.format(nltt[i]))
-#plt.plot(xk*2*np.pi/L*10, nltt, 'o-')
-#plt.xlabel(r'k in units of $nm^{-1}$')
-#plt.ylabel('nltt')
-#plt.show()
 f.close()
 
 
@@ -91,5 +80,3 @@ for j in range(1,101):
     tauft=fft(np.real(corr[1][:int(j*nsnap/100)]))
     tnltt[j-1]=chi[1]/(tauft[0]*(2*np.pi/L)**2)
 xtau=np.linspace(0,nsnap, 100)
-#plt.plot(xtau, tnltt)
-#plt.show()
