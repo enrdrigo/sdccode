71ce86d6f6d5aae3f1b938017093ec00ea1a031e
enrdrigo <72918800+enrdrigo@users.noreply.github.com>
2021-09-09 18:05:37 +0200

    update more stat

--- staticresponse.py
+++ staticresponse.py
@@ -1,17 +1,22 @@
 from modules import initialize
 from modules import compute
+import os
 
 root = './'
 filename = 'dump1.1fs.lammpstrj'
+fileinit = 'init.dat'
 
 posox = 0.125
 nkpoints = 10
 ntrysnap = -1
-inputcompute = initialize.getinitialize(filename, root, posox, nkpoints, ntrysnap)
+if os.path.exists(root+filename):
+    inputcompute = initialize.getinitialize(filename, root, posox, nkpoints, ntrysnap)
+else:
+    inputcompute = initialize.getinitialize(fileinit, root, posox, nkpoints, ntrysnap)
 
 temp = float(input('temperature:>'))
 
-staticresponse = compute.computestaticresponse(inputcompute['root'], inputcompute['filename'], inputcompute['N'], \
-                                             inputcompute['size'], inputcompute['position of the ox'], \
-                                             inputcompute['number of k'], inputcompute['number of snapshots'], temp)
+staticresponse = compute.computestaticresponse(inputcompute['root'], inputcompute['filename'], inputcompute['N'],
+                                               inputcompute['size'], inputcompute['position of the ox'],
+                                               inputcompute['number of k'], inputcompute['number of snapshots'], temp)
 
